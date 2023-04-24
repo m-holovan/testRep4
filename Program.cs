4457447a1950acda5a8c81d41070248e53bad0aa
m-holovan <nikita2003n@gmail.com>
2023-04-24 11:16:41 +0300

    add first value

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter value a:");
+            int a = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
