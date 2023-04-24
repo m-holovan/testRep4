25e19dc24f5799b1bfcb93628c5a57fa0590d2ff
zenepazes1 <zenepazes@gmail.com>
2023-04-24 11:38:23 +0300

    add new cases

--- Program.cs
+++ Program.cs
@@ -27,6 +27,12 @@
                 case 1:
                     Console.WriteLine("Case 1");
                     break;
+                case 2:
+                    Console.WriteLine("Case 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Case 3");
+                    break;
             }
         }
     }
