339bb95845c36b3c914f367f247c709e572b6eb2
m-holovan <nikita2003n@gmail.com>
2023-04-24 11:18:00 +0300

    add comments

--- Program.cs
+++ Program.cs
@@ -5,12 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            //first value enter
             Console.WriteLine("Enter value a:");
             int a = Convert.ToInt32(Console.ReadLine());
-
+            //second value enter
             Console.WriteLine("Enter value b:");
             int b = Convert.ToInt32(Console.ReadLine());
-
         }
     }
 }
