f32d218fb56c2e3ec3786fa4807ae70f8785fa26
m-holovan <nikita2003n@gmail.com>
2023-04-24 11:17:20 +0300

    add second value

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Enter value a:");
             int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter value b:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
         }
     }
 }
